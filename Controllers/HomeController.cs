1a36377c9ffe55b270c7aae2c372ec602aceda52
15Pawlak <pawlakok@gmail.com>
2025-04-13 11:55:56 +0200

    Pierwszy commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CodeHollow.FeedReader;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Persona01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var feed = await FeedReader.ReadAsync("https://www.filmweb.pl/rss/news");
+            ViewBag.NewsItems = feed.Items.Take(5).ToList();
+            return View();
+        }
+    }
+}
